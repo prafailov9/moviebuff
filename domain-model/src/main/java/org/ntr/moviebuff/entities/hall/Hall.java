7841dd22f9be640037ee62f1d8c2a7c71869bc4f
unknown <plamen.rafailov@yahoo.com>
2019-09-13 15:07:31 +0300

    initial commit

---
+++ domain-model/src/main/java/org/ntr/moviebuff/entities/hall/Hall.java
@@ -0,0 +1,106 @@
+package org.ntr.moviebuff.entities.hall;
+
+import java.util.List;
+import java.util.Objects;
+import org.ntr.moviebuff.entities.screening.Screening;
+import org.ntr.moviebuff.entities.seat.Seat;
+
+/**
+ *
+ * @author Rafailov
+ */
+public class Hall {
+    
+    
+    private Long id;
+    
+    private String hallName;
+    
+    private List<Seat> seats;
+    private Screening screening;
+    
+    public Hall() {
+        
+        
+        
+    }
+    
+    public Hall(Long id, String hallName, List<Seat> seats, Screening screening) {
+        this.id = id;
+        this.hallName = hallName;
+        this.seats = seats;
+        this.screening = screening;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getHallName() {
+        return hallName;
+    }
+
+    public void setHallName(String hallName) {
+        this.hallName = hallName;
+    }
+
+    public List<Seat> getSeats() {
+        return seats;
+    }
+
+    public void setSeats(List<Seat> seats) {
+        this.seats = seats;
+    }
+
+    public Screening getScreening() {
+        return screening;
+    }
+
+    public void setScreening(Screening screening) {
+        this.screening = screening;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.id);
+        hash = 71 * hash + Objects.hashCode(this.hallName);
+        hash = 71 * hash + Objects.hashCode(this.seats);
+        hash = 71 * hash + Objects.hashCode(this.screening);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Hall other = (Hall) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.hallName, other.hallName)) {
+            return false;
+        }
+        if (!Objects.equals(this.seats, other.seats)) {
+            return false;
+        }
+        if (!Objects.equals(this.screening, other.screening)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
+    
+    
+}
---
+++ domain-model/src/main/java/org/ntr/moviebuff/entities/screening/Screening.java
@@ -0,0 +1,86 @@
+package org.ntr.moviebuff.entities.screening;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+import org.ntr.moviebuff.entities.film.Film;
+
+/**
+ *
+ * @author Rafailov
+ */
+public class Screening {
+    
+    private Long id;
+    
+    private List<LocalDateTime> screeningDates;
+
+    private Film film;
+
+    public Screening() {
+        
+    }
+    
+    public Screening(Long id, List<LocalDateTime> screeningDates, Film film) {
+        this.id = id;
+        this.screeningDates = screeningDates;
+        this.film = film;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<LocalDateTime> getScreeningDates() {
+        return screeningDates;
+    }
+
+    public void setScreeningDates(List<LocalDateTime> screeningDates) {
+        this.screeningDates = screeningDates;
+    }
+
+    public Film getFilm() {
+        return film;
+    }
+
+    public void setFilm(Film film) {
+        this.film = film;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 79 * hash + Objects.hashCode(this.id);
+        hash = 79 * hash + Objects.hashCode(this.screeningDates);
+        hash = 79 * hash + Objects.hashCode(this.film);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Screening other = (Screening) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.screeningDates, other.screeningDates)) {
+            return false;
+        }
+        if (!Objects.equals(this.film, other.film)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+}
